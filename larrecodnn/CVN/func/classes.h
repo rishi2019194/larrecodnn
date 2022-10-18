0a577ec18f41697b46c4cdd4d6a7c873a8af27e5
Lynn Garren <garren@fnal.gov>
2022-10-18 12:46:29 -0500

    build with cetmodules

--- larrecodnn/CVN/func/classes.h
+++ larrecodnn/CVN/func/classes.h
@@ -2,17 +2,6 @@
 #include "larrecodnn/CVN/func/PixelMap.h"
 #include "larrecodnn/CVN/func/HitType.h"
 #include "larrecodnn/CVN/func/Result.h"
-#include "lardataobj/RecoBase/Cluster.h"
 
-#include "canvas/Persistency/Common/Assns.h"
+#include "canvas/Persistency/Common/Ptr.h"
 #include "canvas/Persistency/Common/Wrapper.h"
-/*
-template class std::vector<cvn::PixelMap>;
-template class art::Ptr<cvn::PixelMap>;
-template class std::vector<cvn::HType>;
-template class art::Wrapper< std::vector<cvn::PixelMap> >;
-
-template class std::vector<cvn::Result>;
-template class art::Ptr<cvn::Result>;
-template class std::vector<art::Ptr<cvn::Result> >;
-*/
