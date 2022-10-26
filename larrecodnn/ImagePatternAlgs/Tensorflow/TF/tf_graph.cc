3b58fdcb93eabec508117a3c5835e537b8a31de0
nitish-nayak <felarof.nayak@gmail.com>
2022-10-26 14:31:32 -0500

    whoops.. actually forgot to commit these

--- larrecodnn/ImagePatternAlgs/Tensorflow/TF/tf_graph.cc
+++ larrecodnn/ImagePatternAlgs/Tensorflow/TF/tf_graph.cc
@@ -20,10 +20,15 @@
 tf::Graph::Graph(const char* graph_file_name,
                  const std::vector<std::string>& outputs,
                  bool& success,
-                 bool use_bundle)
+                 bool use_bundle,
+                 int ninputs,
+                 int noutputs)
 {
   fUseBundle = use_bundle;
   success = false; // until all is done correctly
+  
+  n_inputs = ninputs;
+  n_outputs = noutputs;
 
   // Force tf to only use a single core so it doesn't eat batch farms
   tensorflow::SessionOptions options;
@@ -60,6 +65,10 @@ tf::Graph::Graph(const char* graph_file_name,
 
   size_t ng = graph_def.node().size();
   fInputName = graph_def.node()[0].name();
+  
+  for (int i = 0; i < n_inputs; ++i) {
+    fInputNames.push_back(graph_def.node()[i].name());
+  }
   //if(fUseBundle) fInputName="serving_default_"+fInputName;
   if (fUseBundle) {
     auto sig_map = fBundle->meta_graph_def.signature_def();
@@ -75,6 +84,7 @@ tf::Graph::Graph(const char* graph_file_name,
     for (auto const& p : inputs) {
       input_keys.push_back(p.first);
       fInputName = p.second.name();
+      fInputNames.push_back(p.second.name());
     }
     std::cout << "tf_graph InputName: " << fInputName << std::endl;
     //auto input = model_def.inputs().at( inputs.front() );
@@ -82,7 +92,11 @@ tf::Graph::Graph(const char* graph_file_name,
   }
 
   // last node as output if no specific name provided
-  if (outputs.empty()) { fOutputNames.push_back(graph_def.node()[ng - 1].name()); }
+  if (outputs.empty()) {
+    for (int i = n_outputs; i > 0; --i) {
+      fOutputNames.push_back(graph_def.node()[ng - i].name());
+    }
+  }
   else // or last nodes with names containing provided strings
   {
     std::string last, current, basename, name;
@@ -242,3 +256,122 @@ std::vector<std::vector<float>> tf::Graph::run(const tensorflow::Tensor& x)
   }
 }
 // -------------------------------------------------------------------
+std::vector<std::vector<std::vector<float>>> tf::Graph::runMulti(
+  const std::vector<std::vector<std::vector<std::vector<float>>>>& x,
+  long long int samples)
+{
+  if ((samples == 0) || x.empty() || x.front().empty() || x.front().front().empty() ||
+      x.front().front().front().empty())
+    return std::vector<std::vector<std::vector<float>>>();
+
+  if ((samples == -1) || (samples > (long long int)x.size())) { samples = x.size(); }
+
+  long long int rows = x.front().size(), cols = x.front().front().size(),
+                depth = x.front().front().front().size();
+
+  std::vector<tensorflow::Tensor> _x;
+
+  // Single-input network
+  if (n_inputs == 1) {
+    _x.push_back(tensorflow::Tensor(tensorflow::DT_FLOAT,
+                                    tensorflow::TensorShape({samples, rows, cols, depth})));
+    auto input_map = _x[0].tensor<float, 4>();
+    for (long long int s = 0; s < samples; ++s) {
+      const auto& sample = x[s];
+      for (long long int r = 0; r < rows; ++r) {
+        const auto& row = sample[r];
+        for (long long int c = 0; c < cols; ++c) {
+          const auto& col = row[c];
+          for (long long int d = 0; d < depth; ++d) {
+            input_map(s, r, c, d) = col[d];
+          }
+        }
+      }
+    }
+  }
+  // Multi-input network
+  else {
+    for (int i = 0; i < depth; ++i) {
+      _x.push_back(tensorflow::Tensor(tensorflow::DT_FLOAT,
+                                      tensorflow::TensorShape({samples, rows, cols, 1})));
+    }
+
+    //tensorflow::Tensor _x(tensorflow::DT_FLOAT, tensorflow::TensorShape({ samples, rows, cols, depth }));
+
+    for (int view = 0; view < depth; ++view) {
+      auto input_map = _x[view].tensor<float, 4>();
+      for (long long int s = 0; s < samples; ++s) {
+        const auto& sample = x[s];
+        for (long long int r = 0; r < rows; ++r) {
+          const auto& row = sample[r];
+          for (long long int c = 0; c < cols; ++c) {
+            const auto& col = row[c];
+            long long int d = view;
+            input_map(s, r, c, 0) = col[d];
+          }
+        }
+      }
+    }
+  }
+
+  return runMulti(_x);
+}
+
+// -------------------------------------------------------------------
+
+std::vector<std::vector<std::vector<float>>> tf::Graph::runMulti(
+  const std::vector<tensorflow::Tensor>& x)
+{
+  std::vector<std::pair<std::string, tensorflow::Tensor>> inputs;
+  for (int i = 0; i < n_inputs; ++i) {
+    inputs.push_back({fInputNames[i], x[i]});
+  }
+
+  /*
+    // print input/outputs
+    for(int i = 0; i<n_inputs; ++i)
+        std::cout << inputs[i].first << std::endl;
+    for(int i = 0; i<n_outputs; ++i)
+        std::cout << fOutputNames[i] << std::endl;
+    */
+  //std::cout << "run session" << std::endl;
+
+  std::vector<tensorflow::Tensor> outputs;
+  auto status = fSession->Run(inputs, fOutputNames, {}, &outputs);
+
+  //std::cout << "out size " << outputs.size() << std::endl;
+
+  if (status.ok()) {
+    size_t samples = 0;
+
+    for (size_t o = 0; o < outputs.size(); ++o) {
+      if (o == 0) { samples = outputs[o].dim_size(0); }
+      else if ((int)samples != outputs[o].dim_size(0)) {
+        throw std::string("TF outputs size inconsistent.");
+      }
+    }
+
+    std::vector<std::vector<std::vector<float>>> result;
+    result.resize(samples, std::vector<std::vector<float>>(outputs.size()));
+
+    for (size_t s = 0; s < samples; ++s) {
+      for (size_t o = 0; o < outputs.size(); ++o) {
+        size_t n = outputs[o].dim_size(1);
+        auto output_map = outputs[o].tensor<float, 2>();
+
+        result[s][o].resize(outputs[o].dim_size(1));
+
+        std::vector<float>& vs = result[s][o];
+        for (size_t i = 0; i < n; ++i) {
+          vs[i] = output_map(s, i);
+        }
+      }
+    }
+
+    return result;
+  }
+  else {
+    std::cout << status.ToString() << std::endl;
+    return std::vector<std::vector<std::vector<float>>>();
+  }
+}
--- larrecodnn/ImagePatternAlgs/Tensorflow/TF/tf_graph.h
+++ larrecodnn/ImagePatternAlgs/Tensorflow/TF/tf_graph.h
@@ -25,12 +25,17 @@ namespace tf {
 
   class Graph {
   public:
+    int n_inputs = 1;
+    int n_outputs = 1;
+    
     static std::unique_ptr<Graph> create(const char* graph_file_name,
                                          const std::vector<std::string>& outputs = {},
-                                         bool use_bundle = false)
+                                         bool use_bundle = false,
+                                         int ninputs = 1,
+                                         int noutputs = 1)
     {
       bool success;
-      std::unique_ptr<Graph> ptr(new Graph(graph_file_name, outputs, success, use_bundle));
+      std::unique_ptr<Graph> ptr(new Graph(graph_file_name, outputs, success, use_bundle, ninputs, noutputs));
       if (success) { return ptr; }
       else {
         return nullptr;
@@ -47,18 +52,27 @@ namespace tf {
       const std::vector<std::vector<std::vector<std::vector<float>>>>& x,
       long long int samples = -1);
     std::vector<std::vector<float>> run(const tensorflow::Tensor& x);
+   
+    // use versions for multiple inputs -- needed for CVN 
+    std::vector<std::vector<std::vector<float>>> runMulti(
+      const std::vector<std::vector<std::vector<std::vector<float>>>>& x,
+      long long int samples = -1);
+    std::vector<std::vector<std::vector<float>>> runMulti(const std::vector<tensorflow::Tensor>& x);
 
   private:
     /// Not-throwing constructor.
     Graph(const char* graph_file_name,
           const std::vector<std::string>& outputs,
           bool& success,
-          bool use_bundle = false);
+          bool use_bundle = false,
+          int ninputs = 1,
+          int noutputs = 1);
 
     tensorflow::Session* fSession;
     bool fUseBundle;
     tensorflow::SavedModelBundle* fBundle;
     std::string fInputName;
+    std::vector<std::string> fInputNames;
     std::vector<std::string> fOutputNames;
   };
 
