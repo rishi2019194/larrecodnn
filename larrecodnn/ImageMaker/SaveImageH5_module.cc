c66013f97569f985c3612437cc3b684c283790ac
Tingjun Yang <tjyang@fnal.gov>
2023-04-04 13:19:30 -0500

    Remove code comments.

--- larrecodnn/ImageMaker/SaveImageH5_module.cc
+++ larrecodnn/ImageMaker/SaveImageH5_module.cc
@@ -27,24 +27,19 @@ namespace dnn {
 class dnn::SaveImageH5 : public art::EDAnalyzer {
 public:
   explicit SaveImageH5(fhicl::ParameterSet const& p);
-  // The compiler-generated destructor is fine for non-base
-  // classes without bare pointers or other resource use.
 
-  // Plugins should not be copied or assigned.
   SaveImageH5(SaveImageH5 const&) = delete;
   SaveImageH5(SaveImageH5&&) = delete;
   SaveImageH5& operator=(SaveImageH5 const&) = delete;
   SaveImageH5& operator=(SaveImageH5&&) = delete;
 
-  // Required functions.
   void analyze(art::Event const& e) override;
   virtual ~SaveImageH5() noexcept;
 
-  // Selected optional functions.
   void beginJob() override;
 
 private:
-  // Declare member data here.
+
   hep_hpc::hdf5::File hdffile;
   std::function<decltype(dnn::saveImage)> saveImage_;
   std::string fHDF5FileName;
@@ -55,22 +50,18 @@ dnn::SaveImageH5::SaveImageH5(fhicl::ParameterSet const& p)
   , saveImage_{art::make_tool<decltype(dnn::saveImage)>(p.get<fhicl::ParameterSet>("imageMaker"),
                                                         "saveImage")}
   , fHDF5FileName(p.get<std::string>("HDF5NAME"))
-// More initializers here.
 {
-  // Call appropriate consumes<>() for any products to be retrieved by this module.
 }
 
 dnn::SaveImageH5::~SaveImageH5() noexcept {}
 
 void dnn::SaveImageH5::analyze(art::Event const& e)
 {
-  // Implementation of required member function here.
   saveImage_(e, hdffile);
 }
 
 void dnn::SaveImageH5::beginJob()
 {
-  // Implementation of optional member function here.
   hdffile = hep_hpc::hdf5::File(fHDF5FileName, H5F_ACC_TRUNC);
 }
 
